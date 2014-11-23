bc19770e7eac3e29679e1d601a3b84784f54da1b
Fedomn <shadowforfiend@gmail.com>
2014-11-23 14:18:19 +0800

    LRUCache

---
+++ code/LRUCache/LRUCache.java
@@ -0,0 +1,39 @@
+package LRUCache;
+/**
+ * 	Design and implement a data structure for Least Recently Used (LRU) cache.
+ *  It should support the following operations: get and set.
+ *	get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ *	set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, 
+ *	it should invalidate the least recently used item before inserting a new item.
+ * 
+ */
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class BestResult extends LinkedHashMap<Integer, Integer> {
+	  private int capacity;
+	
+	  public BestResult(int capacity) {
+	    super(16, 0.75f, true);
+	    this.capacity = capacity;
+	  }
+	  
+	  //重写父类get，为null时范围-1
+	  public Integer get(Object key) {
+	    Integer v = super.get(key);
+	    if (v != null)
+	      return v;
+	    else
+	      return -1;
+	  }
+	  
+	  //重写父类方法，当超过缓存容量时，就删除最老的
+	  public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+	    return size() > capacity;
+	  }
+	
+	  public void set(int key, int value) {
+	    super.put(key, value);
+	  }
+}
