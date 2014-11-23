fc0e96898760f3b1b5e4ef39e2b27aa2710886b3
Fedomn <shadowforfiend@gmail.com>
2014-11-23 14:19:33 +0800

    MinStack

---
+++ code/MinStack/MySolution.java
@@ -0,0 +1,43 @@
+class MinStack {
+    
+	private Node top = new Node(0,null,0);
+	private int pushMin = Integer.MAX_VALUE;
+	
+	class Node{
+		public int data;
+		public Node next;
+		public int min;
+		public Node(int data, Node next, int min) {
+			this.data=data;
+			this.next=next;
+			this.min = min;
+		}
+	}
+	
+    public void push(int x) {
+    	pushMin = top.next == null ? x : Math.min(top.next.min, x);
+    	Node temp = new Node(x,top.next,pushMin);
+    	top.next = temp; 
+    }
+
+    public void pop() {
+    	if(top.next != null){
+        	top.next = top.next.next;
+    	}
+    }
+
+    public int top() {
+    	if(top.next != null){
+    		int data = top.next.data;        	
+        	return  data;
+    	}
+    	return Integer.MIN_VALUE;
+    }
+
+    public int getMin(){
+    	if(top.next != null){
+    		return top.next.min;
+    	}
+    	return Integer.MIN_VALUE;
+    }
+}
