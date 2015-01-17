cecf9ab8f726f1ea2d9d9d1255817b96a8d52ddd
Fedomn <shadowforfiend@gmail.com>
2015-01-17 14:21:06 +0800

    commit

--- src/main/java/MaxPointsOnALine/Point.java
+++ src/main/java/MaxPointsOnALine/Point.java
@@ -1,7 +1,8 @@
 package MaxPointsOnALine;
 
-/**
- * Created by fedomn on 2015/1/17.
- */
 public class Point {
+    int x;
+    int y;
+    Point() { x = 0; y = 0; }
+    Point(int a, int b) { x = a; y = b; }
 }
--- src/test/java/MaxPointsOnALine/MaxPointsOnALineTests.java
+++ src/test/java/MaxPointsOnALine/MaxPointsOnALineTests.java
@@ -1,7 +1,20 @@
 package MaxPointsOnALine;
 
-/**
- * Created by fedomn on 2015/1/17.
- */
+import org.junit.Test;
+
+import static MaxPointsOnALine.MaxPointsOnALine.getMaxPoints;
+import static junit.framework.Assert.assertEquals;
+
 public class MaxPointsOnALineTests {
+    private static Point[] testData = {
+        new Point(-1,-1),
+        new Point(-5,-5),
+        new Point(2,2),
+        new Point(3,3)
+    };
+
+    @Test
+    public void MaxPointsOnALineTest(){
+        assertEquals(4, getMaxPoints(testData));
+    }
 }
